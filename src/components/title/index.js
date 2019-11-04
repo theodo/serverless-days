02ed2bbbda0e571888cc55c754e1ed3727fde9f9
Alexandre Fauquette <alex.fauquette@gmail.com>
2019-11-04 16:30:42 +0100

    add image in title

--- src/components/title/index.js
+++ src/components/title/index.js
@@ -1,18 +1,21 @@
 import React from "react"
 import classes from "./styles.module.scss"
-
+import titleImage from "../../images/titleImage.svg"
 const Title = () => (
   <div className={classes.container}>
-    <h1>
-      Serverless days
-      <br />
-      <span className={classes.location}>Paris 2019</span>
-    </h1>
-    <h2 className={classes.subtitle}>
-      La journée de Conférence dédiée à l’architecture Serverless
-    </h2>
-    <p className={classes.rendezVous}>Rendez-vous TBD</p>
-    {/* <button className={classes.button}>prendre sa place</button> */}
+    <div className={classes.informations}>
+      <h1>
+        Serverless days
+        <br />
+        <span className={classes.location}>Paris 2019</span>
+      </h1>
+      <h2 className={classes.subtitle}>
+        La journée de Conférence dédiée à l’architecture Serverless
+      </h2>
+      <p className={classes.rendezVous}>Rendez-vous TBD</p>
+      {/* <button className={classes.button}>prendre sa place</button> */}
+    </div>
+    <img alt="" src={titleImage} className={classes.titleImage} />
   </div>
 )
 

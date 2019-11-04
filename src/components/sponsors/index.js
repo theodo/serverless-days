da84a589803a045a436f6c61942dd3bb2634af27
Alexandre Fauquette <alex.fauquette@gmail.com>
2019-11-04 16:03:37 +0100

    first sponsors logos

--- src/components/sponsors/index.js
+++ src/components/sponsors/index.js
@@ -1,5 +1,9 @@
 import React from "react"
 import classes from "./styles.module.scss"
+import theodoLogo from "../../images/sponsorsLogo/Theodo.svg"
+import googleLogo from "../../images/sponsorsLogo/Google.svg"
+import awsLogo from "../../images/sponsorsLogo/AWS.svg"
+import microsoftLogo from "../../images/sponsorsLogo/Microsoft.svg"
 
 const Sponsors = ({ id }) => (
   <div id={id} className={classes.container}>
@@ -7,7 +11,20 @@ const Sponsors = ({ id }) => (
       <h2 className={classes.title}>Sponsors</h2>
       <p className={classes.subtitle}>ils nous font confiance</p>
     </div>
-    <div className={classes.sponsors}></div>
+    <ol className={classes.sponsors}>
+      <li>
+        <img src={theodoLogo} alt="Theodo" />
+      </li>
+      <li>
+        <img src={googleLogo} alt="Google" />
+      </li>
+      <li>
+        <img src={awsLogo} alt="Amazone Web Sercices" />
+      </li>
+      <li>
+        <img src={microsoftLogo} alt="Microsoft" />
+      </li>
+    </ol>
     <button className={classes.button}>devenir sponsor</button>
   </div>
 )

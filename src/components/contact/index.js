c6529a0461975b6136343d1f05fd650638f10ae6
Alexandre Fauquette <alex.fauquette@gmail.com>
2019-11-08 16:49:10 +0100

    align padding on talk cards

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -4,21 +4,23 @@ import mailLogo from "../../images/mail.svg"
 import twitterLogo from "../../images/twitterLogo.svg"
 import instaLogo from "../../images/instaLogo.svg"
 const Location = ({ id }) => (
-  <div id={id} className={classes.container}>
-    <h2>Contact</h2>
-    <p>Pour plus d’informations contactez notre équipe d’organisateurs</p>
-    <div className={classes.address}>
-      <div className={classes.email}>
-        <img src={mailLogo} alt="" />
-        <address>
-          <a href="mailto:contac@serverlessdaysfrance.fr">
-            contac@serverlessdaysfrance.fr
-          </a>
-        </address>
-      </div>
-      <div className={classes.socials}>
-        <img src={twitterLogo} alt="Twitter" />
-        <img src={instaLogo} alt="Instagram" />
+  <div id={id} className={classes.bacgroundContainer}>
+    <div className={classes.container}>
+      <h2>Contact</h2>
+      <p>Pour plus d’informations contactez notre équipe d’organisateurs</p>
+      <div className={classes.address}>
+        <div className={classes.email}>
+          <img src={mailLogo} alt="" />
+          <address>
+            <a href="mailto:contac@serverlessdaysfrance.fr">
+              contac@serverlessdaysfrance.fr
+            </a>
+          </address>
+        </div>
+        <div className={classes.socials}>
+          <img src={twitterLogo} alt="Twitter" />
+          <img src={instaLogo} alt="Instagram" />
+        </div>
       </div>
     </div>
   </div>

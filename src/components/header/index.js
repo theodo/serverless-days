ffac796599270c923a982f39dbc7675906b560c0
Benjamin Grandfond <benjamin.grandfond@vestiairecollective.com>
2019-11-19 00:42:03 +0100

    Try to debug eventbrite

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,52 +1,41 @@
-import React, { Component } from "react"
+import React from "react"
 import classes from "./styles.module.scss"
 import logo from "../../images/logo.svg"
 
-class Header extends Component {
-  componentDidMount(){
-    const script = document.createElement('script')
-    script.src = "https://www.eventbrite.com/static/widgets/eb_widgets.js"
-    script.async = true;
-    this.instance.appendChild(script)
-  }
+const Header = () => (
+  <header className={classes.header}>
+    <img src={logo} alt="serverless days paris 2020" />
+    <ol className={classes.shortcuts}>
+      <li>
+        <a href="#talks">talks</a>
+      </li>
+      <li>
+        <a href="#contact">lieu & contact</a>
+      </li>
+      <li>
+        <a href="#sponsor">become a sponsor</a>
+      </li>
+      <li>
+        <button id="eventbrite-widget-modal-trigger-82099189887">Billetterie</button>
+      </li>
+    </ol>
+    <script src="https://www.eventbrite.com/static/widgets/eb_widgets.js"></script>
+    <script type="text/javascript" dangerouslySetInnerHTML={{
+      __html: `
+        var exampleCallback = function() {
+          console.log('Order complete!');
+        };
 
-  render () {
-    return (
-      <header className={classes.header} ref={el => (this.instance = el)}>
-        <img src={logo} alt="serverless days paris 2020" />
-        <ol className={classes.shortcuts}>
-          <li>
-            <a href="#talks">talks</a>
-          </li>
-          <li>
-            <a href="#contact">lieu & contact</a>
-          </li>
-          <li>
-            <a href="#sponsor">become a sponsor</a>
-          </li>
-          <li>
-            <button id="eventbrite-widget-modal-trigger-82099189887">Billetterie</button>
-          </li>
-        </ol>
-        <script async dangerouslySetInnerHTML={{
-        __html: `
-            console.log('appended')
-            var exampleCallback = function() {
-              console.log("Order complete!");
-            };
-
-            window.EBWidgets.createWidget({
-              widgetType: "checkout",
-              eventId: "52766401728",
-              modal: true,
-              modalTriggerElementId: "example-widget-trigger",
-              onOrderComplete: exampleCallback
-            });
-        `
-        }} />
-      </header>
-    )
-  }
-}
+        window.EBWidgets.createWidget({
+          widgetType: 'checkout',
+          eventId: '82099189887',
+          modal: true,
+          modalTriggerElementId: 'eventbrite-widget-modal-trigger-82099189887',
+          onOrderComplete: exampleCallback
+        });
+      `
+    }} />
+  </header>
+)
 
 export default Header

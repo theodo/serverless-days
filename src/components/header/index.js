016980db77964f0fc1dd31a9f4630bb36c588753
Benjamin Grandfond <benjamin.grandfond@vestiairecollective.com>
2019-11-19 01:11:29 +0100

    Francise le menu

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -7,13 +7,13 @@ const Header = () => (
     <img src={logo} alt="serverless days paris 2020" />
     <ol className={classes.shortcuts}>
       <li>
-        <a href="#talks">talks</a>
+        <a href="#talks">programme</a>
       </li>
       <li>
         <a href="#contact">lieu & contact</a>
       </li>
       <li>
-        <a href="#sponsor">become a sponsor</a>
+        <a href="#sponsor">devenir sponsor</a>
       </li>
       <li>
         <button id="eventbrite-widget-modal-trigger-82099189887">Billetterie</button>

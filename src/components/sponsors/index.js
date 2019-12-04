84f24b202b025a79f88eeb7781693eb0ffbfb99e
Benjamin Grandfond <benjamin.grandfond@gmail.com>
2019-12-04 22:20:00 +0100

    Add title for each type of sponsors and move button invitation

--- src/components/sponsors/index.js
+++ src/components/sponsors/index.js
@@ -7,66 +7,75 @@ import microsoftLogo from "../../images/sponsorsLogo/Microsoft.svg"
 
 const Sponsors = ({ id }) => (
   <div id={id} className={classes.container}>
-    <div className={classes.titleContainer}>
-      <h2 className={classes.title}>Sponsors</h2>
-      <p className={classes.subtitle}>Soutenez Serverless Days Paris</p>
+    <div className={classes.listContainer}>
+      <h2 className={classes.title}>Sponsors Platinium</h2>
+      <ol className={classes.sponsors + " " + classes.sponsorsPlatinium}>
+        <li>
+          <img src={theodoLogo} alt="Theodo" />
+        </li>
+        {/* <li>
+          <img src={googleLogo} alt="Google" />
+        </li>
+        <li>
+          <img src={awsLogo} alt="Amazone Web Sercices" />
+        </li>
+        <li>
+          <img src={microsoftLogo} alt="Microsoft" />
+        </li> */}
+      </ol>
     </div>
-    <ol className={classes.sponsors + " " + classes.sponsorsPlatinium}>
-      <li>
-        <img src={theodoLogo} alt="Theodo" />
-      </li>
-      {/* <li>
-        <img src={googleLogo} alt="Google" />
-      </li>
-      <li>
-        <img src={awsLogo} alt="Amazone Web Sercices" />
-      </li>
-      <li>
-        <img src={microsoftLogo} alt="Microsoft" />
-      </li> */}
-    </ol>
-    {/* <ol className={classes.sponsors + " " + classes.sponsorsGold}>
-      <li>
-        <img src={theodoLogo} alt="Theodo" />
-      </li>
-      <li>
-        <img src={googleLogo} alt="Google" />
-      </li>
-      <li>
-        <img src={awsLogo} alt="Amazone Web Sercices" />
-      </li>
-      <li>
-        <img src={microsoftLogo} alt="Microsoft" />
-      </li>
-    </ol> */}
-    {/* <ol className={classes.sponsors + " " + classes.sponsorsSilver}>
-      <li>
-        <img src={theodoLogo} alt="Theodo" />
-      </li>
-      <li>
-        <img src={googleLogo} alt="Google" />
-      </li>
-      <li>
-        <img src={awsLogo} alt="Amazone Web Sercices" />
-      </li>
-      <li>
-        <img src={microsoftLogo} alt="Microsoft" />
-      </li>
-    </ol> */}
-    {/* <ol className={classes.sponsors + " " + classes.sponsorsBronze}>
-      <li>
-        <img src={theodoLogo} alt="Theodo" />
-      </li>
-      <li>
-        <img src={googleLogo} alt="Google" />
-      </li>
-      <li>
-        <img src={awsLogo} alt="Amazone Web Sercices" />
-      </li>
-      <li>
-        <img src={microsoftLogo} alt="Microsoft" />
-      </li>
-    </ol> */}
+    {/* <div className={classes.listContainer}>
+      <h2 className={classes.title}>Sponsors Gold</h2>
+      {/* <ol className={classes.sponsors + " " + classes.sponsorsGold}>
+        <li>
+          <img src={theodoLogo} alt="Theodo" />
+        </li>
+        <li>
+          <img src={googleLogo} alt="Google" />
+        </li>
+        <li>
+          <img src={awsLogo} alt="Amazone Web Sercices" />
+        </li>
+        <li>
+          <img src={microsoftLogo} alt="Microsoft" />
+        </li>
+      </ol>
+    </div>*/}
+    {/* <div className={classes.listContainer}>
+      <h2 className={classes.title}>Sponsors Silver</h2>
+      <ol className={classes.sponsors + " " + classes.sponsorsSilver}>
+        <li>
+          <img src={theodoLogo} alt="Theodo" />
+        </li>
+        <li>
+          <img src={googleLogo} alt="Google" />
+        </li>
+        <li>
+          <img src={awsLogo} alt="Amazone Web Sercices" />
+        </li>
+        <li>
+          <img src={microsoftLogo} alt="Microsoft" />
+        </li>
+      </ol> 
+    </div>*/}
+    {/* <div className={classes.listContainer}>
+      <h2 className={classes.title}>Sponsors Bronze</h2>
+      <ol className={classes.sponsors + " " + classes.sponsorsBronze}>
+        <li>
+          <img src={theodoLogo} alt="Theodo" />
+        </li>
+        <li>
+          <img src={googleLogo} alt="Google" />
+        </li>
+        <li>
+          <img src={awsLogo} alt="Amazone Web Sercices" />
+        </li>
+        <li>
+          <img src={microsoftLogo} alt="Microsoft" />
+        </li>
+      </ol>
+    </div>*/}
+    <p className={classes.subtitle}>Soutenez Serverless Days Paris</p>
     <a href="mailto:paris+sponsoring@serverlessdays.io?subject=I wanna support serverless days paris 2020&body=Tell us if you're interested in being one of the 15 sponsors sharing the spotlight during this first Paris edition! We have 4 different packages that you can sign-up for according to your budget and the visibility you wish to have. We would be happy to walk you through the details." className={classes.button}>devenir sponsor</a>
   </div>
 )

6b882f157a4df64bffd07f49f991ff7d1653e528
Benjamin Grandfond <benjamin.grandfond-ext@adevinta.com>
2019-12-11 13:37:34 +0100

    Make the register button in the title work

--- src/components/title/index.js
+++ src/components/title/index.js
@@ -1,36 +1,44 @@
-import React from "react"
+import React, { Component } from "react"
 import { FormattedMessage } from "gatsby-plugin-intl"
 
 import classes from "./styles.module.scss"
 import titleImage from "../../images/titleImage.svg"
 
-const Title = () => (
-  <div className={classes.backgroundContainer}>
-    <div className={classes.container}>
-      <div className={classes.informations}>
-        <h1>
-          <FormattedMessage id="title.title" />
-          <br />
-          <span className={classes.location}><FormattedMessage id="title.location-year"/></span>
-        </h1>
-        <div className={classes.mobileTitleImageContainer}>
-          <img alt="" src={titleImage} />
+class Title extends Component {
+  click () {
+    document.getElementById('eventbrite-widget-modal-trigger-82099189887').click()
+  }
+
+  render () {
+    return (
+      <div className={classes.backgroundContainer}>
+        <div className={classes.container}>
+          <div className={classes.informations}>
+            <h1>
+              <FormattedMessage id="title.title" />
+              <br />
+              <span className={classes.location}><FormattedMessage id="title.location-year"/></span>
+            </h1>
+            <div className={classes.mobileTitleImageContainer}>
+              <img alt="" src={titleImage} />
+            </div>
+            <h2 className={classes.subtitle}>
+              <FormattedMessage id="title.subtitle"/>
+            </h2>
+            <p className={classes.rendezVous}>
+              <FormattedMessage id="title.rendez-vous.when" />
+              <br />
+              <FormattedMessage id="title.rendez-vous.where" />
+            </p>
+            <button className={classes.button} onClick={ this.click }><FormattedMessage id="title.call-to-action" /></button>
+          </div>
+          <div className={classes.desktopTitleImageContainer}>
+            <img alt="" src={titleImage} />
+          </div>
         </div>
-        <h2 className={classes.subtitle}>
-          <FormattedMessage id="title.subtitle"/>
-        </h2>
-        <p className={classes.rendezVous}>
-          <FormattedMessage id="title.rendez-vous.when" />
-          <br />
-          <FormattedMessage id="title.rendez-vous.where" />
-        </p>
-        <button className={classes.button}><FormattedMessage id="title.call-to-action" /></button>
       </div>
-      <div className={classes.desktopTitleImageContainer}>
-        <img alt="" src={titleImage} />
-      </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
 
 export default Title

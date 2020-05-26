1c6977d0656041096f8bd3486b503d7025009f63
Maxence Lecanu <maxencel@theodo.fr>
2020-05-26 16:06:46 +0200

    add streams section with last video url

--- src/components/streams/index.js
+++ src/components/streams/index.js
@@ -1,8 +1,29 @@
 import React from "react"
 import classes from "./styles.module.scss"
+import { FormattedMessage } from "gatsby-plugin-intl"
 
 const Streams = ({ id }) => (
-  <div id={id} className={classes.backgroundContainer}></div>
+  <div id={id} className={classes.container}>
+    <h2 className={classes.title}>
+      <FormattedMessage id="streams.title" />
+    </h2>
+    <div className={classes.streams}>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube-nocookie.com/embed/eVKHBrrz7jY"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+      <p className={classes.streamDesctription}>
+        <FormattedMessage id="streams.description" />
+      </p>
+      <p className={classes.callToAction}>
+        <FormattedMessage id="streams.callToAction" />
+      </p>
+    </div>
+  </div>
 )
 
 export default Streams

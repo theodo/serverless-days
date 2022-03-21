34187421580907a4c2722919b72491246e38a1e6
Frédéric Barthelet <fredericb@theodo.fr>
2022-03-21 23:16:54 +0100

    Add buy button

--- src/components/title/index.js
+++ src/components/title/index.js
@@ -1,21 +1,10 @@
 import React from "react"
-// { useEffect, useState } from "react"
-// import { Helmet } from "react-helmet"
-import { FormattedMessage, Link } from "gatsby-plugin-intl"
+import { FormattedMessage } from "gatsby-plugin-intl"
 
 import classes from "./styles.module.scss"
-import Countdown from "../countdown"
+import Tickets from "../tickets"
 
 const Title = () => {
-  // const [loaded, setLoaded] = useState(false)
-  // useEffect(() => {
-  //   if (!loaded) {
-  //     setLoaded(true)
-  //   }
-  //   return () => {
-  //     setLoaded(false)
-  //   }
-  // }, [setLoaded])
   return (
     <div className={classes.container}>
       <div className={classes.informations}>
@@ -26,46 +15,13 @@ const Title = () => {
             <FormattedMessage id="title.location-year" />
           </span>
         </h1>
-        {/* <div className={classes.mobileTitleImageContainer}>
-          <img alt="" src={titleImage} />
-        </div> */}
         <h2 className={classes.subtitle}>
           <FormattedMessage id="title.subtitle" />
         </h2>
-        {/* <a
-          title="Logiciel billetterie en ligne"
-          href="https://www.weezevent.com/widget_multi.php?266452.1.1.bo"
-          class="weezevent-widget-integration"
-          target="_blank"
-          data-src="https://www.weezevent.com/widget_multi.php?266452.1.1.bo"
-          data-width="100%"
-          data-height="100%"
-          data-id="multi"
-          data-resize="1"
-          data-npb="0"
-          data-width_auto="1"
-        >
-          Billetterie Weezevent
-        </a>
-        {loaded ? (
-          <Helmet>
-            <script
-              async
-              type="text/javascript"
-              src="https://www.weezevent.com/js/widget/min/widget.min.js"
-            ></script>
-          </Helmet>
-        ) : (
-          <></>
-        )} */}
       </div>
       <div className={classes.countdown}>
-        <Countdown></Countdown>
+        <Tickets />
       </div>
-
-      {/* <div className={classes.desktopTitleImageContainer}>
-        <img className={classes.desktopTitleImage} alt="" src={titleImage} />
-      </div> */}
     </div>
   )
 }

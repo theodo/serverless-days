383c525d5017a94f5f3d6517dcc9faef097d01ab
valentinbeggi <valentinbe@theodo.fr>
2022-05-15 22:35:25 +0200

    :sparkles: add sponsor modal

--- src/components/sponsors/index.js
+++ src/components/sponsors/index.js
@@ -1,5 +1,4 @@
-import React from "react"
-import Modal from "react-modal"
+import React, { useState, useCallback } from "react"
 import { FormattedMessage } from "gatsby-plugin-intl"
 import classes from "./styles.module.scss"
 import theodoLogo from "../../images/sponsorsLogo/Theodo.svg"
@@ -7,38 +6,64 @@ import awsLogo from "../../images/sponsorsLogo/AWS.svg"
 import ovrseaLogo from "../../images/sponsorsLogo/ovrsea.png"
 import lumigoLogo from "../../images/sponsorsLogo/lumigo.png"
 import zenikaLogo from "../../images/sponsorsLogo/zenika.png"
+import Dialog from "@material-ui/core/Dialog"
+import DialogContent from "@material-ui/core/DialogContent"
+import close from "../../images/close.svg"
 
-const ModalStyle = {
-  overlay: {
-    position: "fixed",
-    top: 50,
-    left: 50,
-    right: 50,
-    bottom: 50,
-  },
-  content: {
-    position: "absolute",
-    top: "40px",
-    left: "40px",
-    right: "40px",
-    bottom: "200px",
-    border: "1px solid #ccc",
-    overflow: "auto",
-    borderRadius: "4px",
-    outline: "none",
-    padding: "20px",
-  },
+const nameImageMapping = {
+  theodo: { image: theodoLogo, url: "https://theodo.com" },
+  aws: { image: awsLogo, url: "https://aws.amazon.com" },
+  lumigo: { image: lumigoLogo, url: "https://lumigo.io" },
+  ovrsea: { image: ovrseaLogo, url: "https://ovrsea.com" },
+  zenika: { image: zenikaLogo, url: "https://zenika.com" },
+}
+
+const SponsorModal = ({ closeModal, modalData }) => {
+  return (
+    <Dialog open onClose={closeModal} fullWidth>
+      <div className={classes.close}>
+        <button onClick={closeModal}>
+          <img src={close} alt="exit" />
+        </button>
+      </div>
+      <a
+        style={{ alignSelf: "center" }}
+        href={nameImageMapping[modalData].url}
+        target="_blank"
+      >
+        <img
+          className={classes.modalLogo}
+          src={nameImageMapping[modalData].image}
+          alt={modalData}
+        />
+      </a>
+      <DialogContent className={classes.modalContainer}>
+        <FormattedMessage
+          id={`sponsors.${modalData}`}
+          values={{ br: <br /> }}
+        />
+      </DialogContent>
+    </Dialog>
+  )
 }
 
 const Sponsors = ({ id }) => {
-  const [modalIsOpen, setIsOpen] = React.useState(false)
-  function openModal() {
-    setIsOpen(true)
-  }
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const [modalData, setModalData] = useState(null)
 
-  function closeModal() {
-    setIsOpen(false)
-  }
+  const closeModal = useCallback(() => {
+    setModalIsOpen(false)
+    setModalData(null)
+  }, [setModalData, setModalIsOpen])
+
+  const openModal = useCallback(
+    name => {
+      setModalIsOpen(true)
+      setModalData(name)
+    },
+    [setModalData, setModalIsOpen]
+  )
+
   return (
     <div className={classes.container}>
       <div className={classes.titleContainer}>
@@ -59,72 +84,54 @@ const Sponsors = ({ id }) => {
         <div className={classes.sponsor}>
           <p className={classes.sponsorType}>Platinum</p>
           <div className={classes.inLineSponsors}>
-            <a href="https://www.theodo.fr/" target="_blank" >
-              <img
-                className={(classes.logo, classes.bigLogo)}
-                src={theodoLogo}
-                alt="Theodo"
-              />
-            </a>
-            <a href="https://aws.amazon.com/" target="_blank" >
-              <img
-                className={(classes.logo, classes.bigLogo)}
-                src={awsLogo}
-                alt="AWS"
-              />
-            </a>
+            <img
+              className={(classes.logo, classes.bigLogo)}
+              src={theodoLogo}
+              alt="Theodo"
+              onClick={() => openModal("theodo")}
+            />
+
+            <img
+              className={(classes.logo, classes.bigLogo)}
+              src={awsLogo}
+              alt="AWS"
+              onClick={() => openModal("aws")}
+            />
           </div>
         </div>
         <div className={classes.secondSponsors}>
           <div className={classes.sponsor}>
             <p className={classes.sponsorType}>Gold</p>
-            <a href="https://lumigo.io/" target="_blank" >
-              <img
-                className={(classes.logo, classes.mediumLogo)}
-                src={lumigoLogo}
-                alt="Lumigo"
-              />
-            </a>
+            <img
+              className={(classes.logo, classes.mediumLogo)}
+              src={lumigoLogo}
+              alt="Lumigo"
+              onClick={() => openModal("lumigo")}
+            />
           </div>
           <div className={classes.sponsor}>
             <p className={classes.sponsorType}>Silver</p>
-            <a href="https://www.ovrsea.com/fr/" target="_blank" >
-              <img className={classes.logo} src={ovrseaLogo} alt="Ovrsea" />
-            </a>
+            <img
+              className={classes.logo}
+              src={ovrseaLogo}
+              alt="Ovrsea"
+              onClick={() => openModal("ovrsea")}
+            />
           </div>
         </div>
         <div className={classes.logos}>
           <p className={classes.sponsorType}>Bronze</p>
-          <a href="https://www.zenika.com/" target="_blank" >
-            <img className={(classes.logo, classes.smallLogo)} src={zenikaLogo} alt="Zenika" />
-          </a>
+          <img
+            className={(classes.logo, classes.smallLogo)}
+            src={zenikaLogo}
+            alt="Zenika"
+            onClick={() => openModal("zenika")}
+          />
         </div>
+        {modalIsOpen && (
+          <SponsorModal closeModal={closeModal} modalData={modalData} />
+        )}
       </div>
-      {/* <hr className={classes.inKindLine} />
-      <p className={classes.sponsorType}>Suppliers</p>
-      <div className={classes.logos}>
-        <img
-          className={classes.logoInKind}
-          onClick={openModal}
-          src={mickLogo}
-          alt="Les cafés de Mick"
-        />
-      </div>
-      <div>
-        <Modal
-          isOpen={modalIsOpen}
-          onRequestClose={closeModal}
-          contentLabel="Example Modal"
-          style={ModalStyle}
-        >
-          <a href="https://www.lescafesdemick.fr">
-            <FormattedMessage id="sponsors.mick-title" />
-          </a>
-          <br />
-          <br />
-          <FormattedMessage id="sponsors.mick-desc" />
-        </Modal>
-      </div> */}
     </div>
   )
 }
